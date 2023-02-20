e18959a97e15a292b1e994c68c18c3a22f085955
DiaZa13 <cor191025@uvg.edu.gt>
2023-02-20 17:02:23 -0600

    excercise f

---
+++ parallelPi_f.c
@@ -0,0 +1,49 @@
+//
+// Created by Zaray Corado on 2/20/2023.
+//
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "openmp-use-default-none"
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void piCalculation(double factor, int n, int threads, double* result){
+    double sum = 0.0;
+
+#pragma omp parallel for reduction(+:sum) num_threads(threads)
+    for(int i=0;i<n;i++){
+        factor = (i % 2 == 0 ? 1.0 : -1.0);
+        sum += factor/(2*i+1);
+    }
+
+    *result = 4.0*sum;
+}
+
+int main(int argc, char* argv[]){
+    double factor =1.0, result=0.0, start, end;
+    int n = 10e6, threads=4;
+
+    if (argc > 1) {
+        n = strtol(argv[1], NULL, 10);
+        threads = strtol(argv[2], NULL, 10);
+    }
+
+    start = omp_get_wtime();
+    piCalculation(factor, n,threads, &result);
+    end = omp_get_wtime();
+
+    FILE *file;
+    file = fopen("../time.txt", "a");
+    fprintf(file,"%f,%.4f\n",result,end - start);
+    fclose(file);
+
+    printf("Con %d hilos, el cualculo de PI %f segundos\n", threads, end - start);
+    printf("Con n= %d, la aproximacion del valor de PI es: %f",n,result);
+
+
+    return 0;
+}
+
+
+
